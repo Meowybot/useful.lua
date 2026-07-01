266ea21c0829eda85373cce39fd4cb6f98a368f2
Meowybot <jorgedidier149@gmail.com>
2026-07-01 17:36:45 -0600

    Update scalepixels.lua

--- lib/scalepixels.lua
+++ lib/scalepixels.lua
@@ -3,20 +3,37 @@ local scale = {}
 local mt = {}
 mt.stretch = {}
 mt.stretch.__index = mt.stretch
-mt.center = {}
-mt.center.__index = mt.center
+-- mt.center = {}
+-- mt.center.__index = mt.center
 
 function scale.new(mode, w, h, ww, wh)
   assert(mt[mode], "Mode does not exist")
-  return setmetatable({
+  local t = setmetatable({
     w = w,
     h = h,
     ww = ww,
     wh = wh
   }, mt[mode])
+  t.from = {
+    x = w/ww,
+    y = h/wh
+  }
+  t.to = {
+    x = ww/w
+    y = wh/h
+  }
+  return t
 end
 
 local s = mt.stretch
-local c = mt.center
+-- local c = mt.center
+
+function s:fromPixels(x,y)
+  return x*self.from[x], y*self.from[y]
+end
+
+function s:toPixels(x,y)
+  return x*self.to[x], y*self.to[y]
+end
 
 return scale
