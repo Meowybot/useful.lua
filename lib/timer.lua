071b7a494d577267bcd780c225d55af306413dbd
Meowybot <jorgedidier149@gmail.com>
2026-07-02 14:53:53 -0600

    Update timer.lua

--- lib/timer.lua
+++ lib/timer.lua
@@ -1,3 +1,28 @@
 local timer
 
+local mt = {
+  timer = {},
+  sequence = {},
+  tween = {}
+}
+
+mt.timer.__index = mt.timer
+mt.sequence.__index = mt.sequence
+mt.tween.__index = mt.tween
+
+function timer.newTimer(time, after, isLoop)
+  return setmetatable({
+    curTime = 0,
+    time = time or 1,
+    after = after or function() end,
+    isLoop = isLoop or false
+  }, mt.timer)
+end
+
+function timer.newSequence()
+end
+
+function timer.newTween()
+end
+
 return timer
